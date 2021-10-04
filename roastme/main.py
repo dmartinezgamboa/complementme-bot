70d51b4da3308dd45f34b54fd980a58bae7ae4c8
Daniel Martinez-Gamboa <dmartinezgamboa@gmail.com>
2021-10-04 09:39:20 -0700

    Make user ID, channel ID and bot token private as environment variables.

--- roastme/main.py
+++ roastme/main.py
@@ -1,6 +1,11 @@
 import discord
 from random import randrange
 from insults import insults
+import os
+
+TOKEN = os.environ['DISCORD_ROASTME_TOKEN']
+STEVEN = os.environ['STEVEN_ID']
+TEST_CHANNEL = os.environ['DISCORD_TEST_CHANNEL']
 
 client = discord.Client()
 
@@ -8,15 +13,24 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+    channel = client.get_channel(TEST_CHANNEL) #test channel ID
+    await channel.send("I'm online b*tches!")
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.content.startswith('&roastmeCommands'):
+        await message.channel.send(
+          "Do I really need to tell you everything? Here you go:\n \
+          $roastme - for randomized\
+          ")
+
     if message.content.startswith('$roastme'):
         await message.channel.send(insults[randrange(len(insults)-1)])
     
     if message.content.startswith('$roastSteven'):
-        await message.channel.send('@Steven (Chubs) is a c*nt')
+        await message.channel.send('<@!%s> is a c*nt.'%STEVEN)
 
-client.run('token goes here')
+client.run(TOKEN)
