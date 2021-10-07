eec71a4bdc9be33c44e456d4ac1b5316d79c76bf
Daniel Martinez-Gamboa <dmartinezgamboa@gmail.com>
2021-10-07 14:45:57 -0700

    Convert main.py code to use discord.ext commands extension to handle text commands

--- roastme/main.py
+++ roastme/main.py
@@ -1,4 +1,4 @@
-import discord
+from discord.ext import commands
 from random import randrange
 from insults import insults
 import os
@@ -7,31 +7,11 @@ TOKEN = os.environ['DISCORD_ROASTME_TOKEN']
 STEVEN = os.environ['STEVEN_ID']
 TEST_CHANNEL = os.environ['DISCORD_TEST_CHANNEL']
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="/")
 
-@client.event
-async def on_ready():
-    testChannel = client.get_channel(int(TEST_CHANNEL)) #test channel ID
-    print('We have logged in as {0.user}'.format(client))
-    print(testChannel)
-    await testChannel.send("I'm online b*tches!")
+@bot.command()
+async def roast(ctx, arg):
+    await ctx.send(arg)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+bot.run(TOKEN)
 
-    if message.content.startswith('$roastmeCommands'):
-        await message.channel.send(
-          "Do I really need to tell you everything? Here you go:\n\
-          $roastmeCommands - for a list of commands\n\
-          $roastme - for randomized insult\n\
-          $roastSteven - to roast Steven specifically")
-
-    if message.content.startswith('$roastme'):
-        await message.channel.send(insults[randrange(len(insults)-1)])
-    
-    if message.content.startswith('$roastSteven'):
-        await message.channel.send('<@!%s> is a c*nt.'%STEVEN)
-
-client.run(TOKEN)
